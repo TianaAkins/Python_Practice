8583fbeae6e8c35818911ef905c21718a382cf3e
Tiana <tiana.aakins95@gmail.com>
2023-04-05 22:10:17 -0500

    changed error message

--- division.py
+++ division.py
@@ -1,9 +1,10 @@
 import sys
 
 def division_cal(num1, num2):
+    assert type(num1) == int, "User must enter integers"
+    assert type(num2) == int, "User must enter integers"
     if num2 == 0:
-        print("Cannot divide by zero")
-        return 0
+        raise ZeroDivisionError("Cannot divide by zero")
     if num2 > num1:
         return 1
     return num1 / num2
