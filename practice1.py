5037e2e4f71af2b7f6827669c64d667cab577213
Tiana <tiana.aakins95@gmail.com>
2023-05-26 02:36:29 -0500

    added new file practice1

---
+++ practice1.py
@@ -0,0 +1,12 @@
+def division_cal(num1, num2):
+    return num1 / num2
+
+def main():
+    num1 = input("Enter number 1: ")
+    num2 = input("Enter number 2: ")
+
+    division_cal(num1, num2)
+
+main()
+
+
